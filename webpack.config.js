d678db171a7b615a8f304b9b1c4d41288fbb7d4b
Will Davies <william.ifor.davies@gmail.com>
2020-04-05 16:31:26 +0100

    add assets processing and add correct src to card symbol component

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
       { test: /\.js$/, use: 'babel-loader' },
       { test: /\.vue$/, use: 'vue-loader' },
       { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+      { test: /\.(png|svg|jpg|gif)$/, use: 'file-loader' },
     ]
   },
   plugins: [
