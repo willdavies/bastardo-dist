a16a75e0273ce17107773908be9fa59b78385e05
Will Davies <william.ifor.davies@gmail.com>
2020-06-01 21:40:42 +0100

    Set heroku server url as API host

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST: '"TBC"',
+  API_HOST: '"wss://bastardo-server.herokuapp.com/"',
   PLAYER_COOKIE_NAME: '"bastardo-player-id"',
   GAME_SESSION_COOKIE_NAME: '"bastardo-active-game-session"',
   DEFAULT_COOKIE_MAX_AGE: '2592000',
