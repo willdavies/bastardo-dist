75d742da4817d5a31d6913832c7bf07a279d59a5
Will Davies <william.ifor.davies@gmail.com>
2020-06-02 20:56:26 +0100

    comitted dist

--- config/prod.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  API_HOST: '"wss://bastardo-server.herokuapp.com/"',
-  PLAYER_COOKIE_NAME: '"bastardo-player-id"',
-  GAME_SESSION_COOKIE_NAME: '"bastardo-active-game-session"',
-  DEFAULT_COOKIE_MAX_AGE: '2592000',
-}
